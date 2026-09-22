2f11dd8daf0a8ac249377827e034b702f967bbe0
dat <becamdaibac@gmail.com>
2026-09-22 14:36:48 +0700

    .

--- lab2.js
+++ lab2.js
@@ -42,3 +42,67 @@ const namess = ["An", "Bình", "Nam", "Hòa", "Lan"];
 for (let i = 0; i < namess.length; i++) {
   console.log(namess[i]);
 }
+
+//object
+const studentt = {
+  id: 1,
+  name: "Nguyễn Văn Ann",
+  age: 20,
+  email: "an@gmail.com",
+  major: "CNTT",
+};
+console.log(studentt.id);
+studentt.id = "ph6868";
+studentt.address = "Ha Nam";
+
+
+//bai 3
+
+const student = {
+  id: 1,
+  name: "Nguyễn Văn An",
+  age: 20,
+  email: "an@gmail.com",
+  major: "CNTT",
+};
+//in object 
+console.log("thong tin object:", student);
+
+//in ten sinh vien
+console.log("ten sinh vien:", student.name);
+
+//in tuoi
+console.log("tuoi sinh vien:", student.age);
+
+//in email
+console.log("email sinh vien: ", student.email);
+
+//thay doi tuoi
+student.age = 23;
+console.log("tuoi sau khi thay doi:", student.age);
+
+//them thuoc tinh phone 
+student.phone = "0917207459";
+console.log("object sau khi them phone:", student.phone);
+
+//bai 4
+const studentsss = [
+  {
+    id: 1,
+    name: "Nguyễn Văn Annn",
+    age: 20,
+  },
+  {
+    id: 2,
+    name: "Trần Văn Bình",
+    age: 21,
+  },
+  {
+    id: 3,
+    name: "Lê Văn Nam",
+    age: 20,
+  },
+];
+
+//in ra danh sach
+console.log("Danh sach sinh vien: ",studentsss);
